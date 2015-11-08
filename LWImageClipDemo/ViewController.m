510e3bd14c63a18acd6d68c64bc09bb30826400a
leo <276680509@qq.com>
2015-11-08 20:26:27 +0800

    ..

--- LWImageClipDemo/ViewController.m
+++ LWImageClipDemo/ViewController.m
@@ -31,7 +31,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 160.0;
+    return 80.0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
